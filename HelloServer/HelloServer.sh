ebe5026213be6c4247cfa6203b6107fdb7bd7e69
Dale Salter <djsalter93@hotmail.com>
2015-05-22 13:45:02 +1000

    Final: Hello server, sh file fixed up

--- HelloServer/HelloServer.sh
+++ HelloServer/HelloServer.sh
@@ -5,13 +5,14 @@ cd src
 # Compiles the server
 if [[ "$1" == "server"  ]]; then
   javac -cp . net/dalesalter/Server.java
-  java -cp . net.dalesalter.Server 9999
+  java -cp . net.dalesalter.Server $2
 fi
 
 # Compiles the Client
 if [[ "$1" == "client"  ]]; then
   javac -cp . net/dalesalter/Client.java
-  java -cp . net.dalesalter.Client 127.0.0.1 9999
+  java -cp . net.dalesalter.Client $2 $3
 fi
 
-
+echo 'Usage Client: client <server IP> <server port>'
+echo 'Usage Server: server <server port>'
